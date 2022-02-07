f95bd9e70e4fdf74ee68bad7278e3d0d4d8f6748
exit_code_0 <61102500+bipin-mishra1@users.noreply.github.com>
2022-02-07 16:23:14 +0530

    initial commit

---
+++ Employee/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MainEmployee;
+namespace MainProgram{
+    class Program{
+        public static void Main(string[] args){
+            PermanentEmployee e1 = new PermanentEmployee("Dev","Patel","23-08-79","01-03-21",55000.0,23000,1000,14000);
+            Console.WriteLine($"Information about First Employee:\n{e1}");
+            e1.GiveRaise(10.0);
+            Console.WriteLine($"Information about First Employee after 10% raise:\n{e1}");
+            PermanentEmployee e2 = new PermanentEmployee("Rahul","Prajapati","23-08-79","01-03-21",45000.0,21000,1200,12000);
+            Console.WriteLine($"Inforamtion about second Employee:\n{e2}");
+            e2.GiveRaise(10.0);
+            Console.WriteLine($"Inforamtion about second Employee after raise:\n{e2}");
+        }
+    }
+}
